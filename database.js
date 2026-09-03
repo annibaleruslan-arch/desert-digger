75b4e4c69c8ae18e5f08a37fd5b006d759e0b80f
annibaleruslan-arch <annibaleruslan@gmail.com>
2026-09-03 20:49:35 +0300

    Delete database.js

--- database.js
+++
@@ -1,6 +0,0 @@
-const ROCKS_DB = [
-    { name: "Галька", color: "#a6a6a6", price: 3, rarity: "Обычный", chance: 79.0, minW: 0.05, maxW: 0.3 },
-    { name: "Кремень", color: "#595959", price: 18, rarity: "Редкий", chance: 20.0, minW: 0.1, maxW: 0.6 },
-    { name: "Изумруд", color: "#2ecc71", price: 250, rarity: "Легендарный", chance: 0.8, minW: 0.02, maxW: 0.2 },
-    { name: "Алмаз", color: "#00d2d3", price: 600, rarity: "Мифический", chance: 0.2, minW: 0.01, maxW: 0.15 }
-];
